5d9441c81b4a98a11f2436fa07ab721aec6b99e8
Alejandro Harb <alejandroharb@gmail.com>
2017-06-15 21:02:47 -0500

    added watson api key

---
+++ src/config/watsonAPI.js
@@ -0,0 +1,1 @@
+export const watsonAPIKey = "5084ab62c09e2d83e6ce50472b225a5cad4d74e3";
---
+++ src/config/watsonAPI_example.js
@@ -0,0 +1,6 @@
+/*
+---TO GET YOUR PROJECT RUNNING---
+1. enter your api KEY
+2. rename this folder watsonAPI.js
+*/
+export const watsonAPIKey = "<Your API KEY HERE>";
