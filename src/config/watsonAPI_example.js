615143a376ff8a491686982397a57ba0254b0914
Alejandro Harb <alejandroharb@gmail.com>
2017-06-16 00:49:36 -0500

    removed unused dependencies

--- src/config/watsonAPI_example.js
+++ src/config/watsonAPI_example.js
@@ -4,3 +4,4 @@
 2. rename this folder watsonAPI.js
 */
 export const watsonAPIKey = "<Your API KEY HERE>";
+export const watson_API_url = `https://gateway-a.watsonplatform.net/visual-recognition/api/v3/classify?api_key=${watsonAPIKey}&version=2016-05-20`;
