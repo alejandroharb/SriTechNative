e6bd0ab62a1aab08dfe235a5b84ee110294ccb25
Alejandro Harb <alejandroharb@gmail.com>
2017-06-16 00:39:39 -0500

    functional watson response

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 import { View, ImagePickerIOS, Image } from 'react-native';
 
+import { watsonKey, watson_API_url} from './config/watsonAPI';
+import request from 'react-native-request';
+import RNFS from 'react-native-fs';
+import axios from 'axios';
+import RNFetchBlob from 'react-native-fetch-blob';
+
 export default class App extends Component {
   constructor(){
     super();
@@ -14,7 +20,17 @@ export default class App extends Component {
     //openSelectDialog(config, successCallBack, errorCallBack);
     ImagePickerIOS.openSelectDialog({}, imageUri => {
       this.setState({ image: imageUri });
+      this.analyzeImage();
     }, error => console.error(error));
+  }
+
+  analyzeImage(){
+    let formData = new FormData();
+    formData.append('images_file', {uri: this.state.image });
+    formData.append('parameters', {uri: RNFS.MainBundlePath + '/myparams.json' })
+    axios.post(watson_API_url, formData)
+      .then( response => { console.log(JSON.stringify(response.data, null, 2))})
+      .catch( error => {console.log(error)});
   }
 
   render(){
--- src/config/watsonAPI.js
+++ src/config/watsonAPI.js
@@ -1,1 +1,2 @@
 export const watsonAPIKey = "5084ab62c09e2d83e6ce50472b225a5cad4d74e3";
+export const watson_API_url = `https://gateway-a.watsonplatform.net/visual-recognition/api/v3/classify?api_key=${watsonAPIKey}&version=2016-05-20`;
