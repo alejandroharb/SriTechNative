af45dbfccce832524a662442a053b34df0a3440f
Peter Hoàng <peterhoang@utexas.edu>
2017-06-21 19:22:49 -0500

    Changes

--- android/app/src/main/java/com/sritechapp/MainApplication.java
+++ android/app/src/main/java/com/sritechapp/MainApplication.java
@@ -4,9 +4,8 @@ import android.app.Application;
 
 import com.facebook.react.ReactApplication;
 import com.oblador.vectoricons.VectorIconsPackage;
-import com.lwansbrough.RCTCamera.RCTCameraPackage;
-import com.RNFetchBlob.RNFetchBlobPackage;
 import com.rnfs.RNFSPackage;
+import com.lwansbrough.RCTCamera.RCTCameraPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -28,9 +27,8 @@ public class MainApplication extends Application implements ReactApplication {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
             new VectorIconsPackage(),
-            new RCTCameraPackage(),
-            new RNFetchBlobPackage(),
-            new RNFSPackage()
+            new RNFSPackage(),
+            new RCTCameraPackage()
       );
     }
   };
